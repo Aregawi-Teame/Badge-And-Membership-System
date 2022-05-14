70869f86e30489b64cea36c8d6c46f0e257ee2d1
abdurrahimk <abdurrahimk@ABDURRAHIM>
2022-05-14 16:35:32 -0500

    test commit

--- badge-and-membership-system/src/main/java/com/membership/BadgeAndMembershipSystemApplication.java
+++ badge-and-membership-system/src/main/java/com/membership/BadgeAndMembershipSystemApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BadgeAndMembershipSystemApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		System.out.println("test commit");
 		SpringApplication.run(BadgeAndMembershipSystemApplication.class, args);
 	}
 

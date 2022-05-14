4f090fc11ba89994d0fcbcfa71a8e85ccd1e7778
Aregawi-Teame <aregawiyeheys@gmail.com>
2022-05-14 18:30:09 -0500

    Merge branch 'main' of https://github.com/Aregawi-Teame/Badge-And-Membership-System.git into main

--- badge-and-membership-system/src/main/java/com/membership/domain/DayType.java
+++ badge-and-membership-system/src/main/java/com/membership/domain/DayType.java
@@ -1,5 +1,5 @@
 package com.membership.domain;
 
 public enum DayType {
-	DINING_HALL, MEDITATION_HALL,FLYING_HALL,CLASSROOM,GYMNASIUM,DORMITORY
+	
 }
--- badge-and-membership-system/src/main/java/com/membership/domain/LocationType.java
+++ badge-and-membership-system/src/main/java/com/membership/domain/LocationType.java
@@ -1,5 +1,5 @@
 package com.membership.domain;
 
 public enum LocationType {
-
+	DINING_HALL, MEDITATION_HALL,FLYING_HALL,CLASSROOM,GYMNASIUM,DORMITORY
 }

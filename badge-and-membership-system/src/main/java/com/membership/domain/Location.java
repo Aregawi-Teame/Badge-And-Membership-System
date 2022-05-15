30e15236b3c5d6cb09a60a862ddbdd2563889b0b
z-fsehaye <zfsehaye@miu.edu>
2022-05-15 13:47:59 -0500

    created transaction and location (end to end)

--- badge-and-membership-system/src/main/java/com/membership/domain/Location.java
+++ badge-and-membership-system/src/main/java/com/membership/domain/Location.java
@@ -1,5 +1,30 @@
 package com.membership.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
 public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long locationId;
+
+    private String name;
+
+    private String description;
+
+    private int capacity;
+
+    @Enumerated(EnumType.STRING)
+    private LocationType locationType;
 
 }
--- badge-and-membership-system/src/main/java/com/membership/domain/Transaction.java
+++ badge-and-membership-system/src/main/java/com/membership/domain/Transaction.java
@@ -1,5 +1,28 @@
 package com.membership.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long transactionId;
+
+    private LocalDate dateTime;
+
 
 }

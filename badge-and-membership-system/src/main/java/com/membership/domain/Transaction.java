64166a082b783ef62bbc6dd417103a8571e529f0
z-fsehaye <zfsehaye@miu.edu>
2022-05-15 14:21:37 -0500

    updated transaction class

--- badge-and-membership-system/src/main/java/com/membership/domain/Transaction.java
+++ badge-and-membership-system/src/main/java/com/membership/domain/Transaction.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @AllArgsConstructor
@@ -23,6 +20,10 @@ public class Transaction {
     private long transactionId;
 
     private LocalDate dateTime;
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
 
 
 }

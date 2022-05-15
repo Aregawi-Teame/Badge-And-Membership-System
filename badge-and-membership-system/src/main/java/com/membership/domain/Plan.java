1e37721c10d58d7768483c0db29f1d3a424b9525
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2022-05-15 12:32:16 -0500

    Implemented Plan and Role domain entities.

--- badge-and-membership-system/src/main/java/com/membership/domain/Plan.java
+++ badge-and-membership-system/src/main/java/com/membership/domain/Plan.java
@@ -1,5 +1,63 @@
 package com.membership.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Plan {
 
+    @Id
+    private int id;
+    private String name;
+    private String description;
+    private boolean isLimited;
+    private int limit;
+    @OneToMany
+    @JoinTable(name="plan_role",
+            joinColumns = {@JoinColumn(name="Plan_id")},
+            inverseJoinColumns = {@JoinColumn(name="Role_id")}
+    )
+    private Set<Role> roles;
+
+    public Plan(int id, String name, String description, boolean isLimited, int limit, Set<Role> roles) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.isLimited = isLimited;
+        this.limit = limit;
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "Plan{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", isLimited=" + isLimited +
+                ", limit=" + limit +
+                ", roles=" + roles +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Plan)) return false;
+        Plan plan = (Plan) o;
+        return getId() == plan.getId() && isLimited() == plan.isLimited() && getLimit() == plan.getLimit() && getName().equals(plan.getName()) && getDescription().equals(plan.getDescription()) && getRoles().equals(plan.getRoles());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getDescription(), isLimited(), getLimit(), getRoles());
+    }
 }
--- badge-and-membership-system/src/main/java/com/membership/domain/Role.java
+++ badge-and-membership-system/src/main/java/com/membership/domain/Role.java
@@ -1,6 +1,45 @@
 package com.membership.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Role {
-	
+    @Id
+	private int id;
+    private String name;
+
+    public Role(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role)) return false;
+        Role role = (Role) o;
+        return getId() == role.getId() && getName().equals(role.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
 }

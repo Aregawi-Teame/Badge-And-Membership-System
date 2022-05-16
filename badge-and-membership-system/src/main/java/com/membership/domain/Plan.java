b99fa4d7792f0dea21a49c4567dbe33a83a51e4c
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2022-05-16 11:51:05 -0500

    Implemented Plan and Role entities based on the new agreed design...As of Monday meeting

--- badge-and-membership-system/src/main/java/com/membership/domain/Plan.java
+++ badge-and-membership-system/src/main/java/com/membership/domain/Plan.java
@@ -21,21 +21,29 @@ public class Plan {
     private String description;
     private boolean isLimited;
     private int qouta;
+    @ManyToMany
+    @JoinTable(name="Plan_Location",
+            joinColumns = {@JoinColumn(name="Pla=n_id")},
+            inverseJoinColumns = {@JoinColumn(name="Location_id")}
+    )
+    private Set<Location> locations;
     @OneToMany
-    @JoinTable(name="plan_role",
+    @JoinTable(name="Plan_Role",
             joinColumns = {@JoinColumn(name="Plan_id")},
             inverseJoinColumns = {@JoinColumn(name="Role_id")}
     )
     private Set<Role> roles;
 
-    public Plan(int id, String name, String description, boolean isLimited, int limit, Set<Role> roles) {
+    public Plan(int id, String name, String description, boolean isLimited, int qouta, Set<Location> locations, Set<Role> roles) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.isLimited = isLimited;
-        this.qouta = limit;
+        this.qouta = qouta;
+        this.locations = locations;
         this.roles = roles;
     }
+
     @Override
     public String toString() {
         return "Plan{" +
---
+++ badge-and-membership-system/src/main/java/com/membership/repository/PlanRepository.java
@@ -0,0 +1,7 @@
+package com.membership.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PlanRepository {
+}
